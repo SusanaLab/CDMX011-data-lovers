f0f751af67022400abc8c3351540c59c77454473
Susana Garcia <susanlevart@gmail.com>
2021-08-03 16:14:11 -0500

    añadimos el primer filtro

--- src/data.js
+++ src/data.js
@@ -1,6 +1,43 @@
 // estas funciones son de ejemplo
 
-export const pokemonesType = () => {
-  return 'example';
-};
 
+//Get types
+export function filterType(pokemones,typePokemon){
+  let resultado=pokemones.filter(function(pokemones){
+                  return pokemones.type == typePokemon;
+  } )
+  return resultado
+}
+
+//Get resistant
+export function filterResistant(pokemones, resistantPokemon){
+    let resultado=pokemones.filter(function(pokemones){
+                    return pokemones.resistant == resistantPokemon;
+  })
+   return resultado
+}
+
+//Get weaknesses
+export function filterWeaknesses(pokemones, weaknessesPokemon){
+  let resultado=pokemones.filter(function(pokemones){
+                  return pokemones.weaknesses == weaknessesPokemon;
+})
+ return resultado
+}
+
+//Get "special-attack"
+export function filterSpecialAttack(pokemones, SpecialAttackPokemon){
+  let resultado=pokemones.filter(function(pokemones){
+                  return pokemones.SpecialAttack == SpecialAttackPokemon;
+})
+ return resultado
+}
+
+
+export function filterByName(pokemones, namePokemon){
+
+let result = pokemones.filter(function(pokemones){ 
+             return pokemones.Name == Name;
+})
+return result
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterType } from './data.js';
 
-const pokemones=data.pokemon;
+const pokemones = data.pokemon;
 
 let htmlfinal=""
 
@@ -12,20 +11,40 @@ htmlfinal+=generadordehtml(unoauno)
 
 function generadordehtml(pokemoncito){
   return `<div class="box"> <img src="${pokemoncito.img}"> <h1>Name:${pokemoncito.name}</h1>
-   <h2>Num:${pokemoncito.num}</h2> <p>Type:${pokemoncito.type}</p> <p>Resistan:${pokemoncito.resistant} </p> <p>Weaknesses:${pokemoncito.weaknesses} </p> </div>`
+   <h2>Num:${pokemoncito.num}</h2> <p>Type:${pokemoncito.type}</p> <p>Resistant:${pokemoncito.resistant} </p> <p>Weaknesses:${pokemoncito.weaknesses} </p> </div>`
 }
 
 document.getElementById("root").innerHTML=htmlfinal
 
+const changeType = document.querySelector('.filter');
 
- 
-  const pokemonResistant = pokemones.map(function (pokemones) {
-  
-    // return pokemones.type}
-    console.log(pokemones.type.includes("fire"));
+changeType.addEventListener('change', (event) => {
+    const pokemonsFilter= filterType(pokemones,event.target.value)
+    const htmlPokemons= pokemonsFilter.map((pokemoncito)=>
+    `<div class="box"> <img src="${pokemoncito.img}"> <h1>Name:${pokemoncito.name}</h1>
+    <h2>Num:${pokemoncito.num}</h2> <p>Type:${pokemoncito.type}</p> <p>Resistant:${pokemoncito.resistant} </p> <p>Weaknesses:${pokemoncito.weaknesses} </p> </div>`)
     
+    document.getElementById("root").innerHTML=htmlPokemons
+});
 
-    return pokemonResistant
- 
-  })
-  
+
+
+
+
+/*console.log(filterType(pokemones,"fire"))
+console.log(filterType(pokemones,"grass"))
+console.log(filterType(pokemones,"poison"))
+console.log(filterType(pokemones,"flying"))
+console.log(filterType(pokemones,"water"))
+console.log(filterType(pokemones,"bug"))
+console.log(filterType(pokemones,"normal"))
+console.log(filterType(pokemones,"electric"))
+console.log(filterType(pokemones,"ground"))
+console.log(filterType(pokemones,"figthing"))
+console.log(filterType(pokemones,"psychic"))
+console.log(filterType(pokemones,"ice"))
+console.log(filterType(pokemones,"rock"))
+console.log(filterType(pokemones,"dragon"))
+console.log(filterType(pokemones,"fairy"))
+console.log(filterType(pokemones,"steel"))
+console.log(filterType(pokemones,"dark"))*/
