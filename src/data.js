b0f485b11ccd2e2b066de625153b5cec474e24f3
Susana Garcia <susanlevart@gmail.com>
2021-08-05 08:28:15 -0500

    cambioascend

--- src/data.js
+++ src/data.js
@@ -5,31 +5,9 @@ export function filterType(pokemones,typePokemon){
                  })
   return resultado
 }
-//Filtrado por nombre
-export function filterName(pokemones,namePokemon){
-  let resul=pokemones.filter(function(pokemones){
-                  return pokemones.name == namePokemon;
-                 })
-
-  return resul
-  console.log(resul)
-}
 
 
 
 
-export const changeOrder = (x, y) => {
-  const myOrderList = x.sort((pokemonName1, pokemonName2) => {
-    if (pokemonName1.name > pokemonName2.name) {
-      const primer = x;
-      return primer;
-    }
-    if (pokemonName1.name < pokemonName2.name) {
-      const second = y;
-      return second;
-    }
-    return 0;
-  }); return myOrderList;
-};
 
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //aqui controlan todo el dom
 import data from './data/pokemon/pokemon.js';
-import { filterType, filterName } from './data.js';
+import { filterType } from './data.js';
 
 
 
@@ -38,9 +38,7 @@ changeType.addEventListener('change', (event) => {
     `<div class="box">
     <img src="${pokemoncito.img}" id="imgPoke">
     <h4>${pokemoncito.num}</h4>
-    <h2></h2>
     <h1>${pokemoncito.name}</h1>
-    <h3></h3>
     <h4>${pokemoncito.generation.name}</h4>
     <p><b>Type:  </b><br>${pokemoncito.type}</p>
     <p><b>Resistant:  </b><br>${pokemoncito.resistant} </p>
@@ -49,6 +47,53 @@ changeType.addEventListener('change', (event) => {
 
     document.getElementById("root").innerHTML=htmlPokemons
 });
-//Orden Ascendente
+//traemos el arreglo nombres
+let arrNames = pokemones.filter(pokemones => pokemones.name);
+//Ordenamos de forma Z-A
+let ordenDes = arrNames.reverse(function (a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+  console.log(ordenDes);
 
-console.log(filterName(pokemones.name))
+//Ordenamos de forma A-Z
+  let ordenaAscend = arrNames.sort(function (a, b) {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+  console.log(ordenaAscend);
+
+// levamos a boton
+const orderAsc = document.getElementById('order');
+orderAsc.addEventListener('change', (event) => {
+  const save = document.getElementById('root');
+  if (value === 'A-Z') {
+    const ascendente = ordenaAscend;
+    const listaAscendente = ascendente.map((pokemoncito) => 
+    `<div class="box">
+    <img src="${pokemoncito.img}" id="imgPoke">
+    <h4>${pokemoncito.num}</h4>
+    <h1>${pokemoncito.name}</h1>
+    <h4>${pokemoncito.generation.name}</h4>
+    <p><b>Type:  </b><br>${pokemoncito.type}</p>
+    <p><b>Resistant:  </b><br>${pokemoncito.resistant} </p>
+    <p><b>Weaknesses:  </b><br>${pokemoncito.weaknesses} </p>
+     </div>
+    `)
+    save.innerHTML = listaAscendente
+
+  }
+});
+
+
+  
